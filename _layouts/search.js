aae4165e607b216eb6b9eb07d4e1753b0950c511
logep <pingbeer69@gmail.com>
2023-07-27 17:44:51 +0800

    Create search.js

---
+++ _layouts/search.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const searchInput = document.getElementById('search-input');
+  const searchResults = document.getElementById('search-results');
+
+  searchInput.addEventListener('input', function() {
+    const query = searchInput.value.toLowerCase();
+    searchResults.innerHTML = '';
+
+    {% for page in site.pages %}
+      {% if  page.url contains '.html' %}
+        const title = "{{ page.name | escape }}".toLowerCase();
+        const content = "{{ page.content | strip_html | escape }}".toLowerCase();
+
+        if (title.includes(query) || content.includes(query)) {
+          const li = document.createElement('li');
+          const a = document.createElement('a');
+
+          a.href = "{{ page.url }}";
+          a.textContent = "{{ page.name | escape }}";
+          li.appendChild(a);
+          searchResults.appendChild(li);
+        }
+      {% endif %}
+    {% endfor %}
+  });
+});
