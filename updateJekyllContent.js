b3b51bfb4d00e4f4a30bc57aa29272464115d59f
logep <pingbeer69@gmail.com>
2023-11-02 13:40:11 +0800

    Update updateJekyllContent.js

--- updateJekyllContent.js
+++ updateJekyllContent.js
@@ -36,14 +36,15 @@ ${fileContent}
 `;
 
     // 添加导航链接和页面内容
-    navigation.push(`<li><a href="/${file}.html">${file}</a></li>`);
+   // navigation.push(`<li><a href="/${file}.html">${file}</a></li>`);
+     navigation.push(`\n  - ${file}: /${file}.html`);
     pageContents.push(pageContent);
   }
 
   // 更新 Jekyll 导航文件
   const navigationFile = '_data/navigation.yml';
-  fs.appendFileSync(navigationFile, `links:\n  - ${navigation.join('\n  - ')}`);
-
+ // fs.appendFileSync(navigationFile, `\n  - ${navigation.join('\n  - ')}`);
+ fs.appendFileSync(navigationFile, `${navigation.join('')}`);
   // 更新 Jekyll 页面文件
   const jekyllFolder = '_posts'; // 你的 Jekyll 页面文件夹
   for (let i = 0; i < pageContents.length; i++) {
