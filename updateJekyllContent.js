6a458dde017a0a3a7d83d63d5905094417062f86
logep <pingbeer69@gmail.com>
2023-11-02 09:07:42 +0800

    Create updateJekyllContent.js

---
+++ updateJekyllContent.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+
+// 定义多个文件夹路径
+const folderPaths = [
+  'tampermonkey',
+  'tools'
+];
+
+// 遍历多个文件夹路径
+for (const folderPath of folderPaths) {
+  updateJekyllContent(folderPath);
+}
+
+// 更新 Jekyll 内容的函数
+function updateJekyllContent(folderPath) {
+  // 读取文件夹内的文件列表
+  const files = fs.readdirSync(folderPath);
+
+  // 生成导航链接和页面内容
+  const navigation = [];
+  const pageContents = [];
+
+  for (const file of files) {
+    const filePath = path.join(folderPath, file);
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+
+    // 根据文件内容生成页面内容
+    const pageContent = `
+---
+layout: default
+title: ${file}
+---
+
+${fileContent}
+`;
+
+    // 添加导航链接和页面内容
+    navigation.push(`<li><a href="/${file}.html">${file}</a></li>`);
+    pageContents.push(pageContent);
+  }
+
+  // 更新 Jekyll 导航文件
+  const navigationFile = '_data/navigation.yml';
+  fs.writeFileSync(navigationFile, `links:\n  - ${navigation.join('\n  - ')}`);
+
+  // 更新 Jekyll 页面文件
+  const jekyllFolder = '_posts'; // 你的 Jekyll 页面文件夹
+  for (let i = 0; i < pageContents.length; i++) {
+    const pageFileName = `${i + 1}-${files[i]}.md`;
+    fs.writeFileSync(path.join(jekyllFolder, pageFileName), pageContents[i]);
+  }
+
+  console.log(`Updated Jekyll content for folder: ${folderPath}`);
+}
